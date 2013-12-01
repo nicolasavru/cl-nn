e3f2fb415f75de9d7b3adc495c2363231011210a
Nicolas Avrutin <nicolasavru@gmail.com>
2013-12-01 00:26:17 -0500

    fix back propagation learning

--- cl-nn.lisp
+++ cl-nn.lisp
@@ -49,7 +49,7 @@
           (push (make-neuron :in-weights in-weights :g g :dg dg :in fixed-input
                              :fixed-input fixed-input :bias-weight bias-weight)
                 layer)))
-      (setf (aref network pos) layer)
+      (setf (aref network pos) (reverse layer))
       (incf pos))
     network))
 
@@ -114,22 +114,23 @@
       (setf inputs (subseq line 0 num-inputs))
       (setf outputs (subseq line num-inputs))
       (push (list inputs outputs) examples))
-    examples))
+    (reverse examples)))
 
 
-(defun back-prop-learning (training-data network &key (alpha 0.1))
+(defun back-prop-learning (training-data network &key (alpha 0.1) (epochs 100))
   "Train NETWORK on TRAINING-DATA using back-propogation. Training
   data is in the form ((inputs) (outputs))."
   (do ((num-layers (length network))
        (epoch 0 (1+ epoch)))
-      ((= epoch 100) network)
+      ((= epoch epochs) network)
     (dolist (datum training-data)
       ;; set input layer outputs
       (map nil #'(lambda (neuron x)
-                   (setf (neuron-in neuron) x))
+                   (setf (neuron-a neuron) x))
            (aref network 0) (car datum))
       ;; propogate inputs forward
       (map-iota #'(lambda (n)
+                    ;; (print n)
                     (let ((layer (elt network n))
                           (prev-layer (elt network (1- n)))
                           (i 0))
@@ -137,10 +138,12 @@
                         (setf (neuron-in neuron)
                               (* (neuron-fixed-input neuron)
                                  (neuron-bias-weight neuron)))
+                        (setf i 0)
                         (dolist (input-neuron prev-layer)
                           (incf (neuron-in neuron)
                                 (* (elt (neuron-in-weights neuron) i)
-                                   (neuron-a input-neuron))))
+                                   (neuron-a input-neuron)))
+                          (incf i))
                         (setf (neuron-a neuron)
                               (funcall (neuron-g neuron) (neuron-in neuron))))))
                 (1- num-layers) :start 1)
@@ -157,25 +160,26 @@
                           (i 0))
                       (dolist (neuron layer)
                         ;; compute delta
+                        (setf (neuron-delta neuron) 0.0)
                         (dolist (output-neuron next-layer)
                           (incf (neuron-delta neuron)
                                 (* (elt (neuron-in-weights output-neuron) i)
-                                   (neuron-delta output-neuron)))
-                          (incf i))
+                                   (neuron-delta output-neuron))))
                         (setf (neuron-delta neuron)
                               (* (funcall (neuron-dg neuron) (neuron-in neuron))
                                  (neuron-delta neuron)))
-                        ;; update weights to next layer from current neuron
-                        (incf (neuron-bias-weight neuron)
+                      (incf i))
+                      ;; update input weights of next-layer
+                      (dolist (output-neuron next-layer)
+                        (incf (neuron-bias-weight output-neuron)
                               (* alpha
-                                 (neuron-fixed-input neuron)
-                                 (neuron-delta neuron)))
-                        (dolist (output-neuron next-layer)
-                          (setf (neuron-in-weights output-neuron)
-                                (map '(vector float)
-                                     #'(lambda (w neuron)
-                                         (+ w (* alpha
-                                                 (neuron-a neuron)
-                                                 (neuron-delta output-neuron))))
-                                     (neuron-in-weights neuron) layer))))))
-                (- num-layers 2) :start (- num-layers 2) :step -1))))
+                                 (neuron-fixed-input output-neuron)
+                                 (neuron-delta output-neuron)))
+                        (setf (neuron-in-weights output-neuron)
+                              (map '(vector float)
+                                   #'(lambda (w neuron)
+                                       (+ w (* alpha
+                                               (neuron-a neuron)
+                                               (neuron-delta output-neuron))))
+                                   (neuron-in-weights output-neuron) layer)))))
+                (- num-layers 1) :start (- num-layers 2) :step -1))))
